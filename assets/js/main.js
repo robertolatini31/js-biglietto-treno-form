c447e326a858bb9481149ee0f60d0b8ea88adc88
robertolatini31 <robertolatini31@gmail.com>
2022-03-25 14:44:38 +0100

    initial section calculator

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+
+//funzione calcolo prezzo biglietto
+
+// prendere età utente
+
+const elementUserName = document.getElementById("user_name").value;
+
+console.log(`il nome utente è ${elementUserName}`);
+
+// prendere km da fare
+
+const elementUserKm = document.getElementById("km_to_go").value;
+
+console.log(`il nome utente è ${elementUserKm}`);
+
+// prezzo per km 0.21$
+
+const full_price = (elementUserKm * 0.21);
+
+console.log(`prezzo pieno: ${full_price}`);
+
+//prendo età utente
+
+const elementUserAge = document.getElementById("user_age").value;
+
+// imposto lo sconto a zero
+let discount = 0; 
+
+if (user_age < 18) {
+    discount = ((full_price * 20) / 100);
+    // calcolo lo sconto per minorenni
+    console.log(`sconto minorenni ottenuto: ${discount}`);
+} else if (user_age > 65) {
+    discount = ((full_price * 40) / 100);
+    // calcolo lo sconto per over 65
+    console.log(`sconto over 65 ottenuto: ${discount}`);
+}
+
+// calcolo costo finale biglietto
+const final_price = (full_price - discount);
+
+console.log(`prezzo biglietto calcolato: ${final_price.toFixed(2)}`);
